4c3bc6bb347c527d9624d65b076e55f4d7efb015
Christopher Schreiner <git@infanf.com>
2023-07-24 23:44:56 +0200

    chore: initial setup firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CollectionReference, Firestore, Timestamp, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'animagic';
+  items$: Observable<AnimagicEvent[]>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'events') as CollectionReference<AnimagicEvent>;
+    this.items$ = collectionData(itemCollection);
+  }
+}
+
+export interface AnimagicEvent {
+  start: Timestamp;
+  end: Timestamp;
+  title: string;
+  description: string;
+  location: string;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'animagic-2023',
+    appId: '1:762348427246:web:7c6b2b792a4187443960ff',
+    databaseURL: 'https://animagic-2023-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'animagic-2023.appspot.com',
+    apiKey: 'AIzaSyBC3y9BRHyDz4-EtrG4mkMbGl34qfcezW8',
+    authDomain: 'animagic-2023.firebaseapp.com',
+    messagingSenderId: '762348427246',
+  },
+};
